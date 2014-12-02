34f7985c76a1b0e32c2ea56c89957678bb924f33
dukegus <dukegus68@yahoo.com>
2014-12-01 21:30:36 -0500

    simple email form w/ validation(php)

---
+++ inc/footer.inc.php
@@ -0,0 +1,8 @@
+<footer>
+    <h3>Footer</h3>
+    <nav>
+        <a href="#" title="" class="home">home</a>
+        <a href="#" title="" class="about">about</a>
+        <a href="#" title="" class="current contact">contact</a>
+    </nav>
+</footer>
